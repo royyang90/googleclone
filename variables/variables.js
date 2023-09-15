7b048c7f722814f74421f9c6020f76bd7767c7df
roy yang <royyang90@gmail.com>
2023-09-15 16:45:35 +0900

    js커밋

---
+++ variables/variables.js
@@ -0,0 +1,14 @@
+const user = {
+    name: 'John',
+    age: 30,
+    address: null,
+    phonenumber: '1234567890'
+};
+
+const keys = Object.keys(user);
+const values = Object.values(user);
+const entries = Object.entries(user);
+
+// console.log(keys);
+// console.log(values);
+console.log(entries);
